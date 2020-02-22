93162cb89021493625ad994a3687c57bf2ae196d
João Bruno <jnnlbruno@gmail.com>
2020-02-22 22:48:42 +0000

    Added some warning comments to permissions for future reference

--- sqtrex/permissions.py
+++ sqtrex/permissions.py
@@ -1,5 +1,9 @@
 from rest_framework import permissions
 
+# WARNING: There's a bug in django when you use OR |
+# with a custom permission that only implements has_object_permission
+# It will basically not work if that's the case
+# Checkout this: https://github.com/encode/django-rest-framework/issues/7117
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
@@ -9,6 +13,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
         return obj.user == request.user
 
+# WARNING: see comments above
 class IsUserOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
