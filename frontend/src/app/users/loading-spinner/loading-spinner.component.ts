f91ae4a4dc85bac1ce5d4b044a080fecd680cbdb
João Bruno <jnnlbruno@gmail.com>
2020-05-31 15:24:41 +0100

    Now positioning loading indicator on top left corner on top of hamburger menu

--- frontend/src/app/users/loading-spinner/loading-spinner.component.ts
+++ frontend/src/app/users/loading-spinner/loading-spinner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-loading-spinner',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent implements OnInit {
+  faCircleNotch = faCircleNotch;
 
   constructor() { }
 
