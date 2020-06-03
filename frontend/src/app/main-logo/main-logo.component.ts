b56feebcc4f991893e2ebeae28703cd6ca4f07b5
João Bruno <jnnlbruno@gmail.com>
2020-06-03 02:08:20 +0100

    Added main logo component and replaced the scattered logos on login, signup and terms with it; Added auto focus directive and now autofocusing on all components where it made sense

---
+++ frontend/src/app/main-logo/main-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-logo',
+  templateUrl: './main-logo.component.html',
+  styleUrls: ['./main-logo.component.css']
+})
+export class MainLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
