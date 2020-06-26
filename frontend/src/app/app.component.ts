e2e669724d16db1ede1807487d65545e0e8163ee
João Bruno <jnnlbruno@gmail.com>
2020-06-26 05:04:35 +0100

    Added splash/loading screen to cordova app and android webapp

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, AfterViewInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'sukuwatto';
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    if (environment.application) {
+      // if we're on mobile then I'm displaying a loader element that I'll remove 
+      // after the view initialized
+      let loader = this.renderer.selectRootElement('#loader');
+      this.renderer.setStyle(loader, 'display', 'none');
+    }
+  }
 }
