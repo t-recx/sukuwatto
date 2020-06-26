9c8f8fa1ff42ec2a80c995f84e76a3b5a5e3dd78
João Bruno <jnnlbruno@gmail.com>
2020-06-26 03:07:46 +0100

    Added a monkeypatching script so the events are wired correctly with cordova - angular and the back button works as it should

--- frontend/build-android-dev.sh
+++ frontend/build-android-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ng build --configuration=android-dev && sed  '/<body>/a <script src="assets/android/platform_www/cordova.js"></script>' dist/frontend/index.html > index.html.tmp && mv index.html.tmp dist/frontend/index.html 
+ng build --configuration=android-dev && sed  '/<body>/a <script src="assets/android/angular-cordova.js"></script><script src="assets/android/platform_www/cordova.js"></script>' dist/frontend/index.html > index.html.tmp && mv index.html.tmp dist/frontend/index.html 
