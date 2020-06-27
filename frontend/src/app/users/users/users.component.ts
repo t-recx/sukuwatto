891961acd270906edcc3ae5eef74f2250f0ba821
João Bruno <jnnlbruno@gmail.com>
2020-06-27 01:53:35 +0100

    Now unsubscribing from loading service on destroy

--- frontend/src/app/users/users/users.component.ts
+++ frontend/src/app/users/users/users.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { LoadingService } from '../loading.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
   menuDropDownVisible: boolean = false;
   faBars = faBars;
   faTimes = faTimes;
   loading = false;
+  loadingSubscription: Subscription;
 
   constructor(
     public authService: AuthService, 
@@ -21,8 +23,12 @@ export class UsersComponent implements OnInit {
     public route: ActivatedRoute, 
     loadingService: LoadingService,
     ) { 
-      loadingService.state.subscribe(s => this.loading = s);
+      this.loadingSubscription = loadingService.state.subscribe(s => this.loading = s);
     }
+
+  ngOnDestroy(): void {
+    this.loadingSubscription.unsubscribe();
+  }
 
   ngOnInit() {
   }
