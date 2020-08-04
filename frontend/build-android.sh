5cade6b8ca344a2acaf0420b172bb1b4ea166087
João Bruno <jnnlbruno@gmail.com>
2020-08-04 03:10:46 +0100

    Trying something out

--- frontend/build-android.sh
+++ frontend/build-android.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ng build --configuration=android && sed  '/<body>/a <script src="assets/android/angular-cordova.js"></script><script src="assets/android/platform_www/cordova.js"></script>' dist/frontend-android/index.html > index.html.tmp && mv index.html.tmp dist/frontend-android/index.html 
+ng build --configuration=android && sed  '/<body>/a <script src="assets/android/angular-cordova.js"></script><script src="assets/android/platform_www/cordova.js"></script>' dist/frontend/index.html > index.html.tmp && mv index.html.tmp dist/frontend/index.html 
