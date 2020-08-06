6708a1e44ab367e43b2ca53d670ba20bd588442f
João Bruno <jnnlbruno@gmail.com>
2020-08-06 22:28:24 +0000

    Resolve "Add more detail to index page"

---
+++ frontend/src/app/jumbotron/jumbotron.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-jumbotron',
+  templateUrl: './jumbotron.component.html',
+  styleUrls: ['./jumbotron.component.css']
+})
+export class JumbotronComponent implements OnInit {
+
+  showAuthButtons: boolean = environment.showAuthButtons;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
