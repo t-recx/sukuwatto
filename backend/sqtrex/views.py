22d36fd1c0ea4357e20f3ca4d3784b137d76b365
João Bruno <jnnlbruno@gmail.com>
2020-10-13 19:53:52 +0000

    Resolve "Cordova - check status to decide if error page is shown before navigating to site"

--- backend/sqtrex/views.py
+++ backend/sqtrex/views.py
@@ -1,11 +1,17 @@
 from django.contrib.contenttypes.models import ContentType
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from sqtrex.serializers import ContentTypeSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 class ContentTypeList(generics.ListAPIView):
     queryset = ContentType.objects.all()
     serializer_class = ContentTypeSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['model']
+
+@api_view(['GET'])
+def site_status(request):
+    return Response(status=status.HTTP_204_NO_CONTENT)
