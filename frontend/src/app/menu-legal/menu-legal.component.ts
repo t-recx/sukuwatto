45d91ab462c45f2b9f4050511d6e666ceb6b6291
João Bruno <jnnlbruno@gmail.com>
2020-10-16 03:17:08 +0100

    Added icons to menu legal component

--- frontend/src/app/menu-legal/menu-legal.component.ts
+++ frontend/src/app/menu-legal/menu-legal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBalanceScale, faCookie, faEnvelope, faShieldAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu-legal',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-legal.component.css']
 })
 export class MenuLegalComponent implements OnInit {
+
+  faBalanceScale = faBalanceScale;
+  faCookie = faCookie;
+  faEnvelope = faEnvelope;
+  faShieldAlt = faShieldAlt;
 
   constructor() { }
 
