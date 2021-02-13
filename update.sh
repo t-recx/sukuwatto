dde169d454bba0272453a253e8729f6c6459f19f
João Bruno <jnnlbruno@gmail.com>
2021-02-13 18:13:54 +0000

    Changed update script a bit

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git pull && sudo docker-compose down && sudo docker volume rm sqtrex_frontend-android_data && sudo docker volume rm sqtrex_frontend_data && sudo docker-compose up -d --build && sudo docker-compose exec wsgi python manage.py migrate --noinput
+
+echo '-----------------------------------------------------'
+echo '                     All done!                       '
+echo '-----------------------------------------------------'
+echo ''
+echo 'Other actions you may want to do now:'
+echo '# Seed data:'
+echo 'sudo docker-compose exec wsgi python manage.py loaddata <fixture>'
+echo ''
+echo '# See running processes:'
+echo 'sudo docker-compose ps'
+echo ''
+echo '# See logs:'
+echo 'sudo docker-compose logs'
