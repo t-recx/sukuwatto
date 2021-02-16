b02c5c1ac4bc9ff2b77fc79332462c7a899b218b
João Bruno <jnnlbruno@gmail.com>
2021-02-16 18:39:45 +0000

    Periodic tasks now only executed on minute=0 and hour=0

--- backend/workouts/tasks.py
+++ backend/workouts/tasks.py
@@ -2,14 +2,14 @@ from huey import crontab
 from huey.contrib.djhuey import db_periodic_task, db_task
 from workouts.models import WeeklyLeaderboardPosition, MonthlyLeaderboardPosition, YearlyLeaderboardPosition
 
-@db_periodic_task(crontab(day='1', month='1'))
+@db_periodic_task(crontab(minute='0', hour='0', day='1', month='1'))
 def every_year():
     YearlyLeaderboardPosition.objects.all().delete()
 
-@db_periodic_task(crontab(day='1'))
+@db_periodic_task(crontab(minute='0', hour='0', day='1'))
 def every_month():
     MonthlyLeaderboardPosition.objects.all().delete()
 
-@db_periodic_task(crontab(day_of_week='1'))
+@db_periodic_task(crontab(minute='0', hour='0', day_of_week='1'))
 def every_week():
     WeeklyLeaderboardPosition.objects.all().delete()
