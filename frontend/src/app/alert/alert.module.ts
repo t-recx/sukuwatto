d4ef73d64f6d862c0a9d44841ffcbe67e2a70368
João Bruno <jnnlbruno@gmail.com>
2021-04-17 22:36:46 +0100

    Now translating alert messages

--- frontend/src/app/alert/alert.module.ts
+++ frontend/src/app/alert/alert.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertComponent } from './alert/alert.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
   declarations: [AlertComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule,
   ],
   exports: [AlertComponent]
 })
